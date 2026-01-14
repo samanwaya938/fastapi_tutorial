a342b0d68d0d961000ecb23d47493ca934562020
samanwaya938 <samanwayaghosh938@gmail.com>
2026-01-15 02:14:33 +0530

    Fastapi with Docker

--- auth/auth_database.py
+++ auth/auth_database.py
@@ -1,18 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
 
 
-MYSQL_USER = "root"
-MYSQL_PASSWORD = "123456"
-MYSQL_HOST = "localhost"
-MYSQL_PORT = "3306"
-MYSQL_DATABASE = "fastapi_db"
+MYSQL_USER = os.getenv("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "rootpassword")
+MYSQL_HOST = os.getenv("MYSQL_HOST", "db")
+MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "fastapi_db")
 
 DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 
 ## Connection
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,
+    echo= True,
+    pool_pre_ping= True
+)
 
 ## Session
 SessionLocal = sessionmaker(autoflush=False, autocommit = False, bind=engine)
--- auth/main.py
+++ auth/main.py
@@ -1,8 +1,7 @@
-from h11._readers import Http10Reader
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-import models, schemas, utils
-from auth_database import get_db
+from auth import models, schemas, utils
+from auth.auth_database import get_db
 from jose import jwt
 from datetime import datetime, timedelta
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
